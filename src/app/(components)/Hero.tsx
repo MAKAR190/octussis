f5a3603489bed363383f3c2e46372c57820f2465
MAKAR190 <lutskyimakar@gmail.com>
2025-03-30 19:25:31 +0200

    fixes made

--- src/app/(components)/Hero.tsx
+++ src/app/(components)/Hero.tsx
@@ -10,15 +10,11 @@ export default function Hero() {
   return (
     <>
       <VantaBackground />
-      <div className="h-[50vh] mt-24 lg:mt-10 lg:h-screen">
+      <div className="h-[50vh] mt-24 lg:mt-10 lg:h-[70vh]">
         <div
           id="content"
           className="text-center flex justify-center mx-auto h-full w-[70%] flex-col"
-        >
-          <h1 className="lg:text-9xl uppercase md:text-7xl text-5xl drop-shadow-xl text-black font-medium">
-            Octussis
-          </h1>
-        </div>
+        />
       </div>
     </>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-const navigation = {
-  main: [
-    { name: "Lagos" },
-    { name: "London" },
-    { name: "Geneva" },
-    { name: "Abu Dhabi" },
-  ],
-};
-
 export default function Footer() {
   return (
     <div id="contact" className="relative w-full">
@@ -16,9 +7,9 @@ export default function Footer() {
         <div className="pt-12 flex flex-col max-w-[99%]">
           <a
             className="hover:underline underline-offset-1"
-            href="mailto:trade@octussis.com"
+            href="mailto:contact@octussis.com"
           >
-            trade@octussis.com
+            contact@octussis.com
           </a>
         </div>
       </div>
@@ -26,17 +17,15 @@ export default function Footer() {
         <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
           <nav
             aria-label="Footer"
-            className="place-items-center max-w-xl mx-auto justify-center gap-x-12 gap-y-3 text-xs md:text-sm/6 grid grid-cols-4"
+            className="place-items-center max-w-[10vw] mx-auto justify-center gap-x-12 gap-y-3 text-xs md:text-sm/6 grid grid-cols-3"
           >
-            {navigation.main.map((item) => (
-              <div key={item.name} className="flex items-center">
-                <div className="bg-black h-5 w-[1px] mr-4" />
-                <div className="text-black text-center cursor-pointer hover:underline underline-offset-1">
-                  {item.name}
-                </div>
-                <div className="bg-black h-5 w-[1px] ml-4" />
-              </div>
-            ))}
+            <div className="text-black text-center cursor-pointer hover:underline underline-offset-1">
+              Lagos
+            </div>
+            <div className="bg-black h-5 w-[1px]" />
+            <div className="text-black text-center cursor-pointer hover:underline underline-offset-1">
+              London
+            </div>
           </nav>
           <p className="mt-10 text-center text-sm/6 text-gray-500">
             &copy; 2025 Octussis. All rights reserved.
